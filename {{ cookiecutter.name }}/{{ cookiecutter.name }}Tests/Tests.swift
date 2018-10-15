d2b2d60107285f31bd29e6742a14a2ad77a64706
Alexey Korolev <alphatroya@gmail.com>
2018-10-15 15:50:07 +0300

    Add tests target

---
+++ {{ cookiecutter.name }}/{{ cookiecutter.name }}Tests/Tests.swift
@@ -0,0 +1,12 @@
+//
+//  {{ cookiecutter.name }}
+//  Copyright © {% now 'local', '%Y' %} {{ cookiecutter.organization }}. All rights reserved.
+//
+
+import XCTest
+
+final class Tests: XCTestCase {
+    func testStub() {
+        XCTAssertTrue(true)
+    }
+}
