d890b42a978f85a1f356da2e4f6a9105fe94b333
Alexey Korolev <alphatroya@gmail.com>
2019-01-28 16:35:49 +0300

    Add Configuration.plist file

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -11,6 +11,11 @@ curl 'https://www.gitignore.io/api/swift,macos,fastlane,carthage' > .gitignore
 echo 'Carthage/Checkouts' >> .gitignore
 echo '.idea' >> .gitignore
 echo 'Templates' >> .gitignore
+echo '{{ cookiecutter.name }}/Resources/Configuration.plist' >> .gitignore
+
+password=$(pwgen -s 25 1)
+gpg -c --pinentry-mode loopback --passphrase $password ./Project/Resources/Configuration.plist
+rm -fr ./Project/Resources/Configuration.plist
 
 {% if cookiecutter.ipad == "Yes" %}
 mint run "Nonchalant/AppIcon" appicon Icon.png --output-path {{ cookiecutter.name }}/Resources/Assets.xcassets/AppIcon.appiconset --ipad
@@ -30,3 +35,6 @@ git checkout -b develop
 if which pre-commit >/dev/null; then
     pre-commit install
 fi
+
+echo "Configuration password was copied to the clipboard"
+echo $password | pbcopy
