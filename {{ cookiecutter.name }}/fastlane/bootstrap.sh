82000e7e16f6d1d675cdde8746caa5eeff112111
Alexey Korolev <alphatroya@gmail.com>
2019-01-30 10:26:50 +0300

    Add installation of gpg and pwgen tools to the bootstrap script (#38)

--- {{ cookiecutter.name }}/fastlane/bootstrap.sh
+++ {{ cookiecutter.name }}/fastlane/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-brew install mint carthage libxml2 pre-commit
+brew install mint carthage libxml2 pre-commit gpg pwgen
 mint bootstrap
 mint run carthage/carthage carthage bootstrap --cache-builds --platform ios --no-use-binaries
 pre-commit install
