99640ec71eeea7b5b1cc094a5095d67ed6ecf37f
Alexey Korolev <alphatroya@gmail.com>
2020-06-17 10:13:28 +0300

    Remove extra gesture recognizer logic from ParentVC

--- {{ cookiecutter.name }}/{{ cookiecutter.name }}/Classes/Modules/Parent/ParentVC.swift
+++ {{ cookiecutter.name }}/{{ cookiecutter.name }}/Classes/Modules/Parent/ParentVC.swift
@@ -1,18 +1,6 @@
 import UIKit
 
 class ParentVC: UIViewController {
-    private class ParentPopGestureRecognizerDelegate: NSObject, UIGestureRecognizerDelegate {
-        @objc
-        func gestureRecognizer(
-            _: UIGestureRecognizer,
-            shouldRecognizeSimultaneouslyWith _: UIGestureRecognizer
-        ) -> Bool {
-            return true
-        }
-    }
-
-    private let gestureHandler = ParentPopGestureRecognizerDelegate()
-
     override var preferredStatusBarStyle: UIStatusBarStyle {
         if #available(iOS 13.0, *) {
             return .darkContent
@@ -24,16 +12,6 @@ class ParentVC: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         configureStatusBar()
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-
-        if let navigationController = self.navigationController,
-            let gestureRecognizer = navigationController.interactivePopGestureRecognizer {
-            gestureRecognizer.delegate = gestureHandler
-            gestureRecognizer.isEnabled = navigationController.viewControllers.count > 1
-        }
     }
 
     deinit {
