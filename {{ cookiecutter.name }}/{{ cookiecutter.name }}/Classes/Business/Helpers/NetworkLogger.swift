150ccc34a7b002ecb9a0ea4dbb2062c57631efe5
Alexey Korolev <alphatroya@gmail.com>
2020-06-22 10:28:37 +0300

    Replace static NetworkLogger methods by instance methods

--- {{ cookiecutter.name }}/{{ cookiecutter.name }}/Classes/Business/Helpers/NetworkLogger.swift
+++ {{ cookiecutter.name }}/{{ cookiecutter.name }}/Classes/Business/Helpers/NetworkLogger.swift
@@ -4,18 +4,18 @@ protocol LoggerTargetProtocol {
     var description: String { get }
 }
 
-enum NetworkLogger {
-    static func logTarget(_ target: LoggerTargetProtocol) {
+final class NetworkLogger {
+    func logTarget(_ target: LoggerTargetProtocol) {
         log.debug("TARGET =>> %{public}@", logger: .general, target.description)
     }
 
-    static func logRequest(_ request: URLRequest) {
+    func logRequest(_ request: URLRequest) {
         logMessageBlock {
             log.debug("**REQUEST** =>> %{public}@", logger: .network, request.cURL)
         }
     }
 
-    static func logResponse(_ data: Data?) {
+    func logResponse(_ data: Data?) {
         guard let string = data.flatMap({ String(data: $0, encoding: .utf8) }) else {
             log.debug("**RESPONSE** =>> [EMPTY BODY]", logger: .network)
             return
@@ -25,13 +25,13 @@ enum NetworkLogger {
         }
     }
 
-    static func logError(_ error: Error) {
+    func logError(_ error: Error) {
         logMessageBlock {
             log.error("**RESPONSE ERROR** =>> %{public}@", logger: .network, "\(error)")
         }
     }
 
-    private static func logMessageBlock(_ logClosure: () -> Void) {
+    private func logMessageBlock(_ logClosure: () -> Void) {
         switch ProjectConfiguration.current {
         case .debug:
             logClosure()
