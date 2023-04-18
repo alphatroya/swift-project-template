87226f8fa89d55967cbecbe532dde2f887a3770e
Alexey Korolev <alphatroya@gmail.com>
2023-04-18 10:10:32 +0300

    Add ignore_xcproject cookiecutter generation option

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -8,14 +8,16 @@ make swiftgen
 
 # Download .gitignore file
 curl -L 'https://www.gitignore.io/api/swift,macos,fastlane' > .gitignore
-echo '.idea' >> .gitignore
-echo 'Templates' >> .gitignore
 echo 'fastlane/api-key.json' >> .gitignore
 echo '.cache' >> .gitignore
+{% if cookiecutter.ignore_xcproject == "Yes" %}
+    echo '{{ cookiecutter.name }}.xcodeproj' >> .gitignore
+{% endif %}
 
 make fmt
 make gems
 
+# Stamp current version of xcode in .xcode-version file
 xcodebuild -version | sed 's/Xcode //' | head -n 1 > .xcode-version
 
 git init
